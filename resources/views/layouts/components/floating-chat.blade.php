8360f3547acbfeee640cb287ca81c7e51958e14c
kennethcamacho <46813058+kentuy17@users.noreply.github.com>
2023-06-19 01:41:14 +0800

    Update floating-chat.blade.php

--- resources/views/layouts/components/floating-chat.blade.php
+++ resources/views/layouts/components/floating-chat.blade.php
@@ -11,7 +11,10 @@
       </button>
     </div>
     <ul class="messages">
-      <li class="other">👋 Magandang araw kasabong! kung gusto mo maging isang agent, mag-iwan lamang ng mensahe sa ating official facebook page. <a href="https://www.facebook.com/profile.php?id=100093200857033" target="_BLANK">Sabong Aficionado</a></li>
+      {{-- <li class="other">👋 Magandang araw kasabong! kung gusto mo maging isang agent, mag-iwan lamang ng mensahe sa ating official facebook page. <a href="https://www.facebook.com/profile.php?id=100093200857033" target="_BLANK">Sabong Aficionado</a></li> --}}
+      <li class="other">👋 Magandang araw kasabong! Kung gusto mo maging agent sa ating site, mag-iwan lang po ng mensahe sa ating official fb page. <br>👉
+        <a style="color:var(--bs-blue);text-decoration: underline;" href="https://m.me/100093200857033"
+          target="_blank">Sabong Aficionado</a></li>
       <li class="other other-typing" style="display: none;"><i>typing...</i></li>
     </ul>
     <div class="footer">
