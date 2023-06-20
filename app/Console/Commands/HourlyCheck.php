3839cf6f935e4b4f87e91a53f5ae9d4052162b20
kennethcamacho <46813058+kentuy17@users.noreply.github.com>
2023-06-20 15:14:10 +0800

    cron

---
+++ app/Console/Commands/HourlyCheck.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use Pusher\Pusher;
+
+class HourlyCheck extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:hourly-check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        info("Cron Job running at " . now());
+
+        $auth_key = 'e0c6c349e2fee92e00ca';
+        $secret_key = 'e4aa957c59f83b4f373e';
+        $app_id = '1618692';
+
+        $pusher = new Pusher($auth_key, $secret_key, $app_id,['cluster'=>'ap1']);
+
+        $users = User::get();
+        foreach ($users as $key => $user) {
+            # code...
+            if($user->isOnline()) {
+                $pusher->terminateUserConnections($user->id);
+                $user->update([
+                    'active' => false
+                ]);
+            }
+        }
+    }
+}
