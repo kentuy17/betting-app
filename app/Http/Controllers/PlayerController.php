2b963ff814629d389bf04e822d0facfc4aa8e2c8
kentuy17 <kentuy17@gmail.com>
2024-06-14 11:46:32 +0800

    sync

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -38,7 +38,7 @@ class PlayerController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        // $this->middleware('maintenance');
+        $this->middleware('maintenance');
     }
 
     /**
