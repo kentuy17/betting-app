bb90641db4d0880fcfb5516e668e024886d7bf7b
kentuy17 <kentuy17@gmail.com>
2024-06-19 07:41:06 +0800

    fixed .nav conflict

--- resources/views/operator/transactions.blade.php
+++ resources/views/operator/transactions.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app-sub') @section('additional-styles')
+@extends('layouts.app') @section('additional-styles')
   <link rel="stylesheet" href="{{ asset('css/operator.css') }}">
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <style>
