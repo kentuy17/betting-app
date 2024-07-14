09419d8da00cecedc6bcbd9ba84d711e577fbdbe
kennethcamacho <46813058+kentuy17@users.noreply.github.com>
2024-07-14 23:48:00 +0800

    helpers

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,21 +1,22 @@
 <?php
+
 use App\Models\User;
 
-    function hasAccess($permission) {
-        $permissions = Auth::user()->_user_permissions()->all();
-        if(in_array($permission, $permissions)) {
-            return true;
-        }
-        return false;
+function hasAccess($permission)
+{
+    $permissions = Auth::user()->_user_permissions()->all();
+    if (in_array($permission, $permissions)) {
+        return true;
     }
+    return false;
+}
 
-    function userHasAccess($user_id, $permission) {
-        $user = User::find($user_id);
-        $permissions = $user->_user_permissions()->all();
-        if(in_array($permission, $permissions)) {
-            return true;
-        }
-        return false;
+function userHasAccess($user_id, $permission)
+{
+    $user = User::find($user_id);
+    $permissions = $user->_user_permissions()->all();
+    if (in_array($permission, $permissions)) {
+        return true;
     }
-
-?>
+    return false;
+}
